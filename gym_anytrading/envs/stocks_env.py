2e5e548e99701b1a9b4504569dd9ae86af12a70e
Pavan <pavanagrawal@outlook.com>
2022-04-06 20:25:33 -0500

    Michael's bad code

--- gym_anytrading/envs/stocks_env.py
+++ gym_anytrading/envs/stocks_env.py
@@ -1,10 +1,22 @@
 import numpy as np
 
 from .trading_env import TradingEnv, Actions, Positions
+from enum import Enum
+
+class Actions(Enum):
+    Sell = -1
+    Flat = 0
+    Buy = 1
+
+
+class Positions(Enum):
+    Short = -1
+    Flat = 0
+    Long = 1
+
 
 
 class StocksEnv(TradingEnv):
-
     def __init__(self, df, window_size, frame_bound):
         assert len(frame_bound) == 2
 
@@ -14,6 +26,11 @@ class StocksEnv(TradingEnv):
         self.trade_fee_bid_percent = 0.01  # unit
         self.trade_fee_ask_percent = 0.005  # unit
 
+        self.max_possible_positions = 5  # initializing the max # of active positions
+        self.current_positions = 0  # track # active positions
+        self.current_position_cost_bases = []  # dictionary where keys = numbers corresponding to each position index; values = array of important values (cost bases)
+
+        #self.current_position_weights = [] # array of weights for each position in portfolio
 
     def _process_data(self):
         prices = self.df.loc[:, 'Close'].to_numpy()
@@ -45,14 +62,53 @@ class StocksEnv(TradingEnv):
 
         return step_reward
 
-
     def _update_profit(self, action):
+        current_price =
+        ___ = current_price * self.current_positions - sum(self.current_position_cost_bases)
+        if (action == Actions.Buy.value or action == Actions.Sell.value):
+            net_new_pos = self.current_positions + action
+            if (abs(net_new_pos) < self.max_possible_positions):
+                if position_long and action == Actions.Sell.value:
+                    self.current_position_cost_bases.pop(0)
+                    self.current_positions -= 1
+                    INCREMENT
+                    RUNNING
+                    PNL
+                if position_long and action == Actions.Sell.value: REPEAT
+                OF
+                PREVIOUS
+                self.current_position_cost_bases.append(current_price)
+                self.current_positions += 1
+            if position_short and action == Actions.Sell.value:
+                self.current_position_cost_bases.append(current_price)
+                INCREMENT
+                POS(NEGATIVELY)
+            if position_short and action == Actions.Buy.value:
+                self.current_position_cost_bases.pop(0)
+            self.current_positions -= 1
+            INCREMENT
+            RUNNING
+            PNL
+
+    return cummulative_pnl and hennesey_pnl
+
+
         trade = False
+
+        if ((
+                action == Actions.Buy.value and self.current_positions < self.max_possible_positions)):  # if the requested trade won't exceed the portfolio max size, you can trade
+            trade = True
+            self.current_positions += 1
+
         if ((action == Actions.Buy.value and self._position == Positions.Short) or
-            (action == Actions.Sell.value and self._position == Positions.Long)):
+                (action == Actions.Sell.value and self._position == Positions.Long)):
             trade = True
 
         if trade or self._done:
+            # find value of current positions
+            self.current_position_weights / np.sum(
+                self.current_position_weights)  # normalizing position weights to sum to 1
+
             current_price = self.prices[self._current_tick]
             last_trade_price = self.prices[self._last_trade_tick]
 
